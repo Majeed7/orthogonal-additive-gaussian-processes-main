438a2ae7afab1459e5eb9024061a94f259d21793
Majid Mohammadi <majid.mohammadi690@gmail.com>
2025-01-05 21:22:52 +0100

    u5 bash

---
+++ run_batch.sh
@@ -0,0 +1,34 @@
+
+#!/bin/bash
+
+# SLURM directives (optional, for cluster usage)
+#SBATCH --time=72:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=defq
+#SBATCH --gres=gpu:1
+
+# Change directory to the home folder
+cd ~
+
+# Load CUDA 12.1
+module unload all
+module load cuda12.3/toolkit/12.3
+
+# Activate the Python environment
+source /var/scratch/mmi454/envs/agp-sv/bin/activate
+
+# Print system info
+echo "Python version:"
+python --version
+echo "CUDA version:"
+nvcc --version
+
+# Run the Python script
+echo "Running Python script..."
+python ~/orthogonal-additive-gaussian-processes-main/explanation_synthesized.py
+
+
+# Deactivate the environment
+deactivate
+
